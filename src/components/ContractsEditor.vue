8b4cfcb65ff33924a834f8f31847d5430adeec57
Till Kolter <till.kolter@gmail.com>
2018-03-21 18:53:46 +0100

    Add call contract functionality

--- src/components/ContractsEditor.vue
+++ src/components/ContractsEditor.vue
@@ -1,19 +1,34 @@
 <template>
   <div class="contracts-editor">
-    <h1>Ring Contract compile (Block height: {{height}})</h1>
+    <h1>Sofia Contract compile (Block height: {{height}})</h1>
     <form class="server-form">
       <input placeholder="External route" v-model="externalRoute" />
       <input placeholder="Internal route" v-model="internalRoute" />
-    </form>
-    <form class="compile-form" @submit.prevent="onCompile">
       <input placeholder="Paste your hex private key" type="text" v-model="privateKey" required/>
-      <textarea placeholder="Paste your contract source code" v-model="contractCode" required/>
-      <input type="submit" value="Compile"/>
+      <input placeholder="Paste your account key" type="text" v-model="publicKey" required/>
     </form>
-    <form class="deploy-form" @submit.prevent="onDeploy">
-      <textarea placeholder="Paste your contract byte code" v-model="byteCode" required />
-      <input type="submit" value="Deploy"/>
-    </form>
+    <div class="contracts-deploy-panel">
+      <form class="compile-form" @submit.prevent="onCompile">
+        <textarea placeholder="Paste your contract source code" v-model="contractCode" required/>
+        <input type="submit" value="Compile"/>
+      </form>
+      <form class="deploy-form" @submit.prevent="onDeploy">
+        <textarea placeholder="Paste your contract byte code" v-model="byteCode" required />
+        <input type="submit" value="Deploy"/>
+      </form>
+    </div>
+    <div class="contracts-call-panel">
+      <form class="call-data-form" @submit.prevent="onComputeCallData">
+        <input placeholder="Type function name" v-model="functionName" required>
+        <input placeholder="Type comma separated function arguments" v-model="functionArgs" required>
+        <input type="submit" value="Generate call data" />
+      </form>
+      <form class="call-form" @submit.prevent="onCallFunction">
+        <input placeholder="Paste your contract id" v-model="contractAddress" required />
+        <input placeholder="Paste your call data" v-model="callData" required />
+        <input type="submit" value="Call function" />
+      </form>
+    </div>
     <div>
       {{status}}
     </div>
@@ -27,11 +42,16 @@ export default {
   name: 'ContractsEditor',
   data () {
     return {
-      privateKey: '',
+      privateKey: '3b12b6baed7aaa1a51a27b62e8cf8cecd6c41df094cb9528ec55e996cad72ad8',
+      publicKey: 'ak$3hJ3Jt4uCcgUsZ4dR6TjxAyCqZX2TL7aJudN4qBSwwDsgVq9g9RcKwZQdk22GzBx7n9pNJhbtssLj8apZjZNH8kfu7NQMd',
       contractCode: '',
       byteCode: '',
       height: 0,
       status: '',
+      callData: '',
+      functionName: '',
+      functionArgs: '',
+      contractAddress: '',
       externalRoute: 'http://localhost:3001',
       internalRoute: 'http://localhost:3001/internal',
       client: undefined
@@ -45,14 +65,42 @@ export default {
     },
     async onDeploy () {
       this.status = 'Create transaction object'
-      let data = await this.client.contracts.getCreateTx(this.byteCode)
+      let data = await this.client.contracts.getCreateTx(
+        this.byteCode,
+        {owner: this.publicKey}
+      )
       this.status = 'Sign and send transaction'
+      this.contractAddress = data['contract_address']
       this.client.tx.sendSigned(data.tx, this.privateKey)
       this.status = 'Waiting for transaction to be mined'
       let interval = setInterval(async () => {
         let transaction = await this.client.tx.getTransaction(data['tx_hash'])
         if (transaction['block_height'] !== -1) {
           this.status = `Contract deployed successfully on block ${transaction['block_height']}`
+          clearInterval(interval)
+        }
+      }, 2000)
+    },
+    async onComputeCallData () {
+      let callData = await this.client.contracts.encodeCallData('ring',
+        this.byteCode,
+        this.functionName,
+        this.functionArgs.split(',')
+      )
+      this.callData = callData
+    },
+    async onCallFunction () {
+      let data = await this.client.contracts.getCallTx(
+        this.contractAddress,
+        this.callData,
+        {caller: this.publicKey, amount: 0}
+      )
+      await this.client.tx.sendSigned(data.tx, this.privateKey)
+      this.status = 'Waiting for contract call transaction to be mined'
+      let interval = setInterval(async () => {
+        let transaction = await this.client.tx.getTransaction(data['tx_hash'])
+        if (transaction['block_height'] !== -1) {
+          this.status = `Contract call tx successfully on block ${transaction['block_height']}`
           clearInterval(interval)
         }
       }, 2000)
@@ -81,12 +129,43 @@ export default {
 </script>
 
 <style lang="scss">
-  .contracts-editor {
-    form {
+  input[type="button"] {
+    width: inherit;
+  }
+  .contracts {
+    &-editor {
+      form {
+        display: flex;
+        flex-direction: column;
+        textarea {
+          width: 100%;
+        }
+      }
+    }
+    &-call-panel {
       display: flex;
-      flex-direction: column;
-      textarea {
-        width: 100%;
+      flex-direction: row;
+      form {
+        width: 50%;
+        padding: 8px;
+        margin: 8px;
+        border: 1px solid black;
+        input {
+          margin: 8px;
+        }
+      }
+    }
+    &-deploy-panel {
+      display: flex;
+      flex-direction: row;
+      form {
+        width: 50%;
+        padding: 8px;
+        margin: 8px;
+        border: 1px solid black;
+        input {
+          margin: 8px;
+        }
       }
     }
   }
@@ -102,7 +181,7 @@ export default {
   .compile-form {
     textarea {
       height: 200px;
-      width: 100%;
+      width: 50%;
     }
     input {
       margin: 16px;
