1eaf704b8df222c83cdf456c93eab22714859a8c
HristiyanG <Hristiyan@limechain.tech>
2019-10-30 15:15:15 +0200

    refactor smart contract

--- src/components/contractSrc.js
+++ src/components/contractSrc.js
@@ -1,46 +1,24 @@
 export const contractSource = `contract CryptoHamster =
+   datatype event = NewHamster(indexed int, string, hash)
 
-   record state = {
-      index : int,
-      map_hamsters : map(string, hamster)}
+   record state = { hamsters : map(string, hash), next_id : int }
 
-   record hamster = {
-      id: int,
-      name: string,
-      dna: int}
+   stateful entrypoint init() = { hamsters = {}, next_id = 0 }
 
-   stateful entrypoint init() = 
-      { index = 1,
-         map_hamsters = {}}
+   entrypoint nameExists(name: string) : bool =
+      Map.member(name, state.hamsters)
 
-   stateful entrypoint create_hamster(hamster_name: string) =
-      require(!name_exists(hamster_name), "Name is already taken")
-      let dna : int = generate_random_dna()
-      create_hamster_by_name_dna(hamster_name, dna)
-      
+   stateful entrypoint createHamster(hamsterName: string) =
+      require(!nameExists(hamsterName), "Name is already taken")
+      createHamsterByNameDNA(hamsterName, generateDNA(hamsterName))
+   
+   entrypoint getHamsterDNA(hamsterName: string) : hash =
+      require(nameExists(hamsterName), "Hamster does not exist!")
+      state.hamsters[hamsterName]
 
-   entrypoint name_exists(name: string) : bool =
-      Map.member(name, state.map_hamsters)
-
-   entrypoint get_hamster_dna(name: string) : int =
-      require(name_exists(name), "There is no hamster with that name!")
-      let needed_hamster : hamster = state.map_hamsters[name]
-      needed_hamster.dna
-
-   stateful function create_hamster_by_name_dna(name: string, dna: int) =
-      let new_hamster : hamster = {
-         id = state.index,
-         name = name,
-         dna = dna}
-
-      put(state{map_hamsters[name] = new_hamster})
-      put(state{index = (state.index + 1)})
-
-   function generate_random_dna() : int =
-      get_block_hash_bytes_as_int() - Chain.timestamp + state.index
-      
-
-   function get_block_hash_bytes_as_int() : int =
-      switch(Chain.block_hash(Chain.block_height - 1))
-         None => abort("blockhash not found")
-         Some(bytes) => Bytes.to_int(bytes)`
+   stateful function createHamsterByNameDNA(name: string, dna: hash) =
+      Chain.event(NewHamster(state.next_id, name, dna))
+      put(state{hamsters[name] = dna, next_id = (state.next_id + 1)})
+ 
+   function generateDNA(name : string) : hash =
+      String.sha3(name)`
