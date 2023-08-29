2f2cf80f6ec980b1cd6d157985556387d187a664
radrow <radrowicki@gmail.com>
2023-08-29 17:36:54 +0200

    feat: initial repl implementation

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -3,6 +3,7 @@
     <Account />
     <div v-if="status === Status.CONNECTED">
       <CodeAci />
+      <Repl />
       <div class="flex mt-8 mb-8">
         <Deploy />
         <CallStatic />
@@ -13,6 +14,7 @@
 </template>
 <script setup lang="ts">
 import CodeAci from "./CodeAci.vue";
+import Repl from "./Repl.vue";
 import Deploy from "./Deploy.vue";
 import Account from "./Account.vue";
 import CallStatic from "./CallStatic.vue";
