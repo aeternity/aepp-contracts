b2c38bac916dae43d162888453857c81963e6abe
Philipp <github@piwo.space>
2023-09-12 17:03:03 +0300

    refactor: only establish channel on open, fix code style

--- src/components/Repl.vue
+++ src/components/Repl.vue
@@ -4,7 +4,7 @@
       <div class="relative w-8/12">
         <button
           class="my-2 mr-2 rounded-full bg-black hover:bg-purple-500 text-white p-2 px-4"
-          @click="show = !show"
+          @click="toggleRepl()"
         >
           {{ show ? "Close" : "Open" }}
         </button>
@@ -19,7 +19,7 @@
           <div
             id="repl_output"
             class="p-2 whitespace-pre-wrap text-white bg-black font-mono leading-none h-96 resize-y overflow-auto"
-            v-html="replOutput"
+            v-html="output"
           />
           <!-- eslint-enable -->
         </div>
@@ -30,31 +30,31 @@
           <button
             class="mt-2 rounded-l-full bg-black hover:bg-purple-500 text-white p-2 px-4"
           >
-            {{ replPrompt }}
+            {{ prompt }}
           </button>
           <input
-            v-model="replQuery"
+            v-model="query"
             class="mt-2 bg-black hover:bg-purple-500 text-white p-2 px-4 w-8/12"
-            @keyup.enter="repl_submitQuery()"
-            @keyup.up="repl_historyUp()"
-            @keyup.down="repl_historyDown()"
+            @keyup.enter="submitQuery()"
+            @keyup.up="historyUp()"
+            @keyup.down="historyDown()"
           />
           <button
             class="mt-2 mr-2 rounded-r-full bg-black hover:bg-purple-500 text-white p-2 px-4"
-            @click="repl_submitQuery()"
+            @click="submitQuery()"
           >
             Query
           </button>
         </div>
         <div class="relative w-4/12">
           <input
-            v-model="replContractName"
+            v-model="contractName"
             class="mt-2 rounded-l-full bg-black hover:bg-purple-500 text-white p-2 px-4"
-            @keyup.enter="repl_loadFiles()"
+            @keyup.enter="loadFiles()"
           />
           <button
             class="mt-2 mr-2 rounded-r-full bg-black hover:bg-purple-500 text-white p-2 px-4"
-            @click="repl_loadFiles()"
+            @click="loadFiles()"
           >
             Load Contract
           </button>
@@ -68,7 +68,7 @@ import { storeToRefs } from "pinia";
 import { useContractStore } from "../stores/contractStore";
 import { ref, watch } from "vue";
 
-import { Socket } from "phoenix";
+import { Channel, Socket } from "phoenix";
 import { AnsiUp } from "ansi_up";
 
 const ansiUp = new AnsiUp();
@@ -77,50 +77,93 @@ const contractStore = useContractStore();
 const { compileData, compileResult } = storeToRefs(contractStore);
 
 const show = ref(false);
-const replOutput = ref("");
-const replQuery = ref("");
-const replContractName = ref("");
-const replPrompt = ref("");
+const output = ref("");
+const query = ref("");
+const contractName = ref("");
+const prompt = ref("");
 
 watch(
   compileResult,
   () => {
-    replContractName.value = compileResult.value.data?.aci
+    contractName.value = compileResult.value.data?.aci
       ? JSON.parse(compileResult.value.data?.aci)[0]?.contract.name + ".aes"
       : "";
   },
-  { deep: true }
+  { deep: true },
 );
 
 ////////////////////////////////////////////////////////////////////////////////
 
-const socket = new Socket("wss://repl.prd.aepps.com/socket");
-socket.connect();
-
-const channel = socket.channel("repl_session:lobby", {});
-
+let channel: Channel | null = null;
 let session: string | null = null;
 let lastPrompt = "";
 let lastInput = "";
 let history: string[] = [];
 let historyIndex = 0;
 
-replQuery.value = "";
+function toggleRepl() {
+  show.value = !show.value;
 
-function repl_historyUp() {
+  if (show.value) {
+    const socket = new Socket("wss://repl.prd.aepps.com/socket");
+    socket.connect();
+
+    channel = socket.channel("repl_session:lobby", {});
+
+    session = null;
+    lastPrompt = "";
+    lastInput = "";
+    history = [];
+    historyIndex = 0;
+
+    query.value = "";
+
+    channel.on(
+      "response",
+      (payload: { prompt: string; user_session: string; msg: string }) => {
+        const prompt = payload.prompt;
+        session = payload.user_session || session;
+        const msg = payload.msg.replace(/^\n|\n$/g, "");
+        if (msg) {
+          logResponse(msg);
+        }
+        if (prompt) {
+          updatePrompt(prompt);
+        }
+      },
+    );
+
+    channel.onError(() => alert("Channel error."));
+
+    channel
+      .join()
+      .receive("ok", () => {
+        console.log("Joined aerepl lobby.");
+      })
+      .receive("error", (e) => {
+        console.error(e);
+        updatePrompt("(CHANNEL ERROR)");
+        alert("Could not establish the connection.");
+      });
+  } else {
+    channel?.leave();
+  }
+}
+
+function historyUp() {
   if (historyIndex == history.length) {
-    lastInput = replQuery.value;
+    lastInput = query.value;
   }
   historyIndex -= 1;
-  history_retrieve();
+  historyRetrieve();
 }
 
-function repl_historyDown() {
+function historyDown() {
   historyIndex += 1;
-  history_retrieve();
+  historyRetrieve();
 }
 
-function history_get() {
+function historyGet() {
   historyIndex = Math.min(history.length, Math.max(0, historyIndex));
   if (historyIndex == history.length) {
     return lastInput;
@@ -129,79 +172,47 @@ function history_get() {
   }
 }
 
-function history_retrieve() {
-  replQuery.value = history_get();
+function historyRetrieve() {
+  query.value = historyGet();
 }
 
-function repl_loadFiles() {
+function loadFiles() {
   const contract = compileData.value.contractCode;
-  channel.push("load", {
-    files: [{ filename: replContractName.value, content: contract }],
+  channel?.push("load", {
+    files: [{ filename: contractName.value, content: contract }],
     user_session: session,
   });
 }
 
-function repl_submitQuery() {
-  const query = replQuery.value.trim();
-  channel.push("query", { input: query, user_session: session });
+function submitQuery() {
+  const trimmedQuery = query.value.trim();
+  channel?.push("query", { input: trimmedQuery, user_session: session });
 
-  lastInput = query;
+  lastInput = trimmedQuery;
   historyIndex = history.length;
-  if (history[historyIndex - 1] != query) {
-    history = history.concat(query);
+  if (history[historyIndex - 1] != trimmedQuery) {
+    history = history.concat(trimmedQuery);
     historyIndex += 1;
   }
-  replQuery.value = "";
+  query.value = "";
 }
 
-function log_response(msg: string) {
+function logResponse(msg: string) {
   const txt = ansiUp.ansi_to_html(msg);
   const prompt = lastPrompt ? lastPrompt + "> " : "";
-  replOutput.value += "\n" + prompt + lastInput + "\n" + txt + "\n";
+  output.value += "\n" + prompt + lastInput + "\n" + txt + "\n";
   setTimeout(() => updateScroll(), 10);
 }
 
-function update_prompt(prompt: string) {
-  lastPrompt = prompt;
-  replPrompt.value = prompt + "> ";
+function updatePrompt(updatePrompt: string) {
+  lastPrompt = updatePrompt;
+  prompt.value = updatePrompt + "> ";
 }
 
 function updateScroll() {
   const element = document.getElementById("repl_output");
   if (element) element.scrollTop = element.scrollHeight;
 }
-
-channel.on(
-  "response",
-  (payload: { prompt: string; user_session: string; msg: string }) => {
-    const prompt = payload.prompt;
-    session = payload.user_session || session;
-    const msg = payload.msg.replace(/^\n|\n$/g, "");
-    if (msg) {
-      log_response(msg);
-    }
-    if (prompt) {
-      update_prompt(prompt);
-    }
-  }
-);
-
-channel.onError(() => alert("Channel error."));
-channel.onClose(() => {
-  update_prompt("(CLOSED)");
-  alert("The channel has been closed. Please refresh to start a new session.");
-});
-
-channel
-  .join()
-  .receive("ok", () => {
-    console.log("Joined aerepl lobby.");
-  })
-  .receive("error", (e) => {
-    console.error(e);
-    update_prompt("(CHANNEL ERROR)");
-    alert("Could not establish the connection.");
-  });
 
 ////////////////////////////////////////////////////////////////////////////////
 </script>
